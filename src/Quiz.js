809d63837b629c1c57da413a1dfe825430e62647
Sachin Bhosale <sachinbhosale345@github.com>
2018-06-03 17:33:24 +0530

    Quiz End Component Added

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
+
 let quizData =  require('./quiz_data.json');
 
 class Quiz extends Component{
@@ -11,10 +13,10 @@ class Quiz extends Component{
     }
     render(){
         console.log("aaaaaaaa",quizData);
-        
+        const isQuizEnd = (this.state.quiz_position -1) === quizData.quiz_questions.length;
         return(
             <div>
-                <QuizQuestion quiz_question = {quizData.quiz_questions[(this.state.quiz_position)-1]} />
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question = {quizData.quiz_questions[(this.state.quiz_position)-1]} />}
            </div>
         );
     }
