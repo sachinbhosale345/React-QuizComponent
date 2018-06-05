72f81bfe3746d88f8bf977c8b41a03b0f7ee79ea
Sachin Bhosale <sachinbhosale345@github.com>
2018-06-05 09:47:34 +0530

    minor changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component{
             quiz_position : 1
         }
     }
-    showNextQuestio(){
+    showNextQuestion(){
         this.setState(
             this.state.quiz_position = (this.state.quiz_position) +1
         )
